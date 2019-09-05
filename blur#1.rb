39ebd7a992480e72114c0516e2e30f3b730afc7a
Pete <prs148@hotmail.com>
2019-09-05 03:08:17 +0000

    Initial Commit

---
+++ blur#1.rb
@@ -0,0 +1,40 @@
+
+
+class Image 
+  attr_accessor :image
+  def initialize (image)
+    @image = image 
+
+  end
+
+ 
+  def output_image 
+  
+   @image.each do |array|
+      puts array.join("")
+    end
+  end
+
+  
+end
+
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
+
+
+
+
+
+
+
+
+
+
+
+
