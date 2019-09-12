c7c80442c1e12dd29a573c382fd9fa67bbd965b3
Pete <prs148@hotmail.com>
2019-09-12 20:24:29 +0000

    all challenges complete

---
+++ blur#2.rb
@@ -0,0 +1,66 @@
+
+
+class Image 
+  attr_accessor :image
+  def initialize (image)
+    @image = image 
+
+  end
+
+ 
+  def output_image 
+  
+   @image.each do |array|
+      puts array.join("")
+    end
+  end
+
+
+
+  def transform
+   
+    b = Marshal.load(Marshal.dump(@image))
+      @image.each_with_index do |row, y| 
+        row.each_with_index do |_, x|
+          if b[y][x] == 1 
+              @image[y + 1][x] = 1 unless y == @image.length-1 
+              @image[y - 1][x] = 1 unless y == 0
+              @image[y][x + 1] = 1 unless x == @image.length-1
+              @image[y][x - 1] = 1 unless x == 0
+
+          end    
+      
+        end
+      end  
+  
+   
+    
+  end
+end
+
+
+
+
+
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
+])
+
+image.transform
+image.output_image
+
+
+
+
+
+
+
+
+
+
+
+
